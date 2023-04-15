873847d2f4f7b16eb725c367529ffd2739fefae7
Kai Wittmann <kai@heart-co.de>
2023-04-15 21:49:25 +0200

    feat: invoice navigation

--- src/components/InvoicePageContainer.vue
+++ src/components/InvoicePageContainer.vue
@@ -8,10 +8,19 @@
     <slot />
   </div>
   <div class="h-8 print:hidden"></div>
+  <BaseButton class="fixed left-8 top-8 print:hidden" @click="router.push(`/invoice/${invoiceId}`)">
+    <ChevronLeftIcon class="w-4" />
+  </BaseButton>
 </template>
 <script setup lang="ts">
 import backgroundImage from '@/assets/DIN_5008_Form_B.svg'
 import { useMagicKeys } from '@vueuse/core'
+import BaseButton from './BaseButton.vue'
+import { ChevronLeftIcon } from '@heroicons/vue/20/solid'
+import { useRouter } from 'vue-router'
 
 const { b } = useMagicKeys()
+
+const router = useRouter()
+const { invoiceId } = router.currentRoute.value.params
 </script>
