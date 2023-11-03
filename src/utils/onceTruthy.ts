77615f49fdbc1e1f0f083ad16fa888a4fd2bdae3
Kai Wittmann <kai@heart-co.de>
2023-11-03 12:13:55 +0100

    chore: improve onceTruthy

--- src/utils/onceTruthy.ts
+++ src/utils/onceTruthy.ts
@@ -1,7 +1,7 @@
 import { until } from '@vueuse/core'
 import type { Ref } from 'vue'
 
-export const onceTruthy = async (value: Ref<unknown>, callback: () => void) => {
+export const onceTruthy = async <T>(value: Ref<T>, callback: (value: T & {}) => void) => {
   await until(value).toBeTruthy()
-  callback()
+  callback(value.value!)
 }
