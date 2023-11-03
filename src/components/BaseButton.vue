394f844819e77fe8b7ce69a071e49297100b3106
Kai Wittmann <kai@heart-co.de>
2023-11-03 12:14:17 +0100

    chore: improve BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,25 @@
 <template>
   <button
+    :type="type"
+    :disabled="disabled"
     class="flex justify-center px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    :class="{
+      'opacity-50 cursor-not-allowed': disabled,
+    }"
   >
     <slot />
   </button>
 </template>
+
+<script setup lang="ts">
+import { computed, toRefs } from 'vue'
+
+const props = defineProps<{
+  disabled?: boolean
+  type?: 'button' | 'submit'
+}>()
+
+const { disabled } = toRefs(props)
+
+const type = computed(() => props.type ?? 'button')
+</script>
