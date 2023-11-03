0914ddeca84e94c90f34c33ec723de854025ca1c
Kai Wittmann <kai@heart-co.de>
2023-11-03 12:14:35 +0100

    fix: properly clean up title tag

--- src/views/invoice/InvoicePrintView.vue
+++ src/views/invoice/InvoicePrintView.vue
@@ -12,7 +12,7 @@ import { useCustomer } from '@/api/useCustomer'
 import { useInvoice } from '@/api/useInvoice'
 import { useUserData } from '@/api/useUserData'
 import InvoiceMain from '@/components/InvoiceMain.vue'
-import { computed, toRefs, watch } from 'vue'
+import { computed, onBeforeUnmount, toRefs, watch } from 'vue'
 import { useTitle } from '@vueuse/core'
 
 const props = defineProps<{ invoiceId: number }>()
@@ -23,11 +23,22 @@ const { data: invoiceData } = useInvoice(invoiceId)
 const { data: customerData } = useCustomer(computed(() => invoiceData.value?.customer_id))
 const { data: userData } = useUserData()
 
+const titleText = computed(() => {
+  if (!invoiceData.value || !customerData.value) return ''
+  return `${invoiceData.value.invoice_number} - ${customerData.value.name}`
+})
+
 const title = useTitle()
+const formerTitle = title.value || ''
+onBeforeUnmount(() => {
+  const titleTag = document.querySelector('title')
+  if (!titleTag) return
+  titleTag.textContent = formerTitle
+})
 watch(
-  invoiceData,
+  titleText,
   () => {
-    title.value = `${invoiceData.value?.invoice_number} - ${customerData.value?.name}`
+    title.value = titleText.value
   },
   { immediate: true },
 )
