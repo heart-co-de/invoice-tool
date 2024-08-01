8b106fd57f52ec6f9f7842bbe22b55d0238b726f
Kai Wittmann <kai@heart-co.de>
2024-08-01 18:37:40 +0200

    fix: typescript issues

--- src/components/DropdownCustomer.vue
+++ src/components/DropdownCustomer.vue
@@ -7,7 +7,7 @@
       <ComboboxInput
         class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         @change="query = $event.target.value"
-        :display-value="customer => (customer as Customer).name"
+        :display-value="(customer: Customer) => customer.name"
       />
       <ComboboxButton
         class="absolute inset-y-0 right-0 flex items-center px-2 rounded-r-md focus:outline-none"
